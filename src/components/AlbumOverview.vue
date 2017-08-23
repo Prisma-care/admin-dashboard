ea9f63f4fbd12355f2c6b9425d867e9797adf02e
Michiel Leyman <michiel.leyman@gmail.com>
2017-08-23 12:12:56 +0200

    Add raw card style

--- src/components/AlbumOverview.vue
+++ src/components/AlbumOverview.vue
@@ -1,15 +1,60 @@
 <template>
-  <div class="container">
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="../assets/logo.png" class="image">
+          <div style="padding: 14px;">
+            <span>Yummy hamburger</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">Operating button</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
 
-  </div>
 </template>
 
 <script>
 export default {
   name: 'album-overview',
+  data() {
+    return {
+      currentDate: new Date(),
+    };
+  },
 };
 </script>
 
 <style scoped>
+.time {
+    font-size: 13px;
+    color: #999;
+  }
 
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+
+  .clearfix:after {
+      clear: both
+  }
 </style>
