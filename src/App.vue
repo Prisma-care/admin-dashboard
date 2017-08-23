96a3e4741e5d7ed3420fc63b2f1d84fc597d5338
Michiel Leyman <michiel.leyman@gmail.com>
2017-08-23 12:26:08 +0200

    Use google fonts

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,18 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto|Varela+Round');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2 {
+  font-family: 'Varela Round', Helvetica, Arial, sans-serif;
 }
 </style>
--- src/components/AlbumOverview.vue
+++ src/components/AlbumOverview.vue
@@ -3,7 +3,7 @@
       <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
         <el-card :body-style="{ padding: '0px' }">
           <img src="../assets/logo.png" class="image">
-          <div style="padding: 14px;">
+          <div>
             <span>Yummy hamburger</span>
             <div class="bottom clearfix">
               <time class="time">{{ currentDate }}</time>
@@ -29,32 +29,36 @@ export default {
 
 <style scoped>
 .time {
-    font-size: 13px;
-    color: #999;
-  }
+  font-size: 13px;
+  color: #999;
+}
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.el-card div {
+  padding: 14px;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.button {
+  padding: 0;
+  float: right;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
+.image {
+  width: 100%;
+  display: block;
+}
 
-  .clearfix:after {
-      clear: both
-  }
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both
+}
 </style>
