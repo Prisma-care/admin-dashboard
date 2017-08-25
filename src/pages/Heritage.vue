ed3916debd08993c4baf626e1d49870a95d1ec0e
Michiel Leyman <michiel.leyman@gmail.com>
2017-08-25 15:27:32 +0200

    Add loader and get album info

--- src/pages/Heritage.vue
+++ src/pages/Heritage.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container">
+  <div class="container" v-loading.body="loading">
     <header>
       <div class="">
-        <h1 class="logo">Album title</h1>
+        <h1 class="logo" v-if="!loading">{{ album.title }}</h1>
         <RenameRemoveDropdown @rename="renameAlbum" @remove="removeAlbum" :confirming-removal="confirmingRemoval"></RenameRemoveDropdown>
       </div>
       <el-button class="button" @click="addStory">Add story</el-button>
@@ -11,18 +11,25 @@
 </template>
 
 <script>
-// import * as api from '@/api/';
+import * as api from '@/api/';
 import RenameRemoveDropdown from '@/components/RenameRemoveDropdown';
 
 export default {
   components: { RenameRemoveDropdown },
   data() {
     return {
+      album: null,
+      loading: true,
       confirmingRemoval: false
     };
   },
   mounted() {
-    console.log(this.$route.params.id);
+    api.getDefaultAlbum(this.$route.params.id).then((res) => {
+      this.album = res.data.response;
+      this.loading = false;
+    }).catch((err) => {
+      console.log(err);
+    });
   },
   methods: {
     addStory() {
