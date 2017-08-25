f95a974cc23dfdb37ca67c20103ca8a852447dd4
Michiel Leyman <michiel.leyman@gmail.com>
2017-08-25 13:49:20 +0200

    Add Rename/Remove scaffolding to Heritage page

--- src/pages/Heritage.vue
+++ src/pages/Heritage.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="container">
     <header>
-      <h1 class="logo">Album title</h1>
-      <el-button class="button" @click="addAlbum">Add story</el-button>
+      <div class="">
+        <h1 class="logo">Album title</h1>
+        <RenameRemoveDropdown @rename="renameAlbum" @remove="removeAlbum" :confirming-removal="confirmingRemoval"></RenameRemoveDropdown>
+      </div>
+      <el-button class="button" @click="addStory">Add story</el-button>
     </header>
   </div>
 </template>
 
 <script>
 // import * as api from '@/api/';
+import RenameRemoveDropdown from '@/components/RenameRemoveDropdown';
 
 export default {
+  components: { RenameRemoveDropdown },
+  data() {
+    return {
+      confirmingRemoval: false
+    };
+  },
   mounted() {
     console.log(this.$route.params.id);
+  },
+  methods: {
+    addStory() {
+      // add a story
+    },
+    renameAlbum() {
+      //
+    },
+    removeAlbum() {
+      //
+    }
   }
 };
 </script>
 
 <style scoped>
+h1 {
+  padding-right: 10px;
+}
 </style>
