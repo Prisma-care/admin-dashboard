efa5df486014ba8b21228d4d945c3dd5066e1b2e
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-04 16:52:38 +0200

    Remove opn all together

--- build/dev-server.js
+++ build/dev-server.js
@@ -81,10 +81,7 @@ devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri).then(() => {
-      // closed
-    })
-    .catch(err => console.log(err));
+    //
   }
   _resolve()
 })
